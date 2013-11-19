bba1e02b3c9d0d2933164e373836a2f364183f69
Ivan Georgiev <ivan@jonan.info>
2013-11-19 13:35:47 +0200

    - [study] filtering works nicely;

--- scripts/toxstudy.js
+++ scripts/toxstudy.js
@@ -31,7 +31,7 @@ var jToxStudy = {
     return "" + data.result.loValue + data.result.unit;
   },
   
-  ensureCategory: function(tab, category, name, count) {
+  ensureCategory: function(tab, category, name) {
     var self = this;
 
     var theCat = $('.' + category + ' .jtox-study-table')[0];
@@ -40,10 +40,33 @@ var jToxStudy = {
       theCat.removeAttribute('id');
       tab.appendChild(theCat);
       $(theCat).addClass(category);
+      
+      // install the click handler for fold / unfold
+      theCat.onclick = function(e) {
+        $(this).toggleClass('folded');
+      };
+      
+      // now install the filter box handler. It delays the query a bit and then spaws is to all tables in the tab.
+      var filterTimeout = null;
+      var fFilter = function (ev) {
+        if (!!filterTimeout)
+          clearTimeout(filterTimeout);
+
+        var field = ev.currentTarget;
+        filterTimeout = setTimeout(function() {
+          var tabList = tab.getElementsByClassName('jtox-study-table');
+          for (var t = 0, tlen = tabList.length; t < tlen; ++t) {
+            $(tabList[t]).dataTable().fnFilter(field.value);
+          }
+        }, 300);
+      };
+      
+      var filterEl = tab.getElementsByClassName('jtox-study-filter')[0];
+      filterEl.onkeydown = fFilter;
     }
     
     var titleEl = theCat.getElementsByClassName('jtox-study-title')[0];
-    titleEl.innerHTML = name + " (" + count + ")";
+    titleEl.innerHTML = name + " (0)";
     return theCat;
   },
   
@@ -54,7 +77,7 @@ var jToxStudy = {
     if (!$(theTable).hasClass('dataTable')) {
 
       var colDefs = [
-        { "sClass": "middle", "mData": "index", "mRender": function(data, type, full) { return '#' + data; } } // Number...
+        { "sClass": "center middle", "bSearchable": false, "mData": "index", "mRender": function(data, type, full) { return '#' + data; } } // Number...
       ];
       
       // start filling it
@@ -77,11 +100,11 @@ var jToxStudy = {
 
       // use it to put parameters...
       putAGroup(study.parameters, function(p) {
-        return { "sClass" : "middle", "mData" : "parameters." + p, "sDefaultContent": "-"};
+        return { "sClass" : "center middle", "mData" : "parameters." + p, "sDefaultContent": "-"};
       });
       // .. and conditions
       putAGroup(study.effects[0].conditions, function(c){
-        return { "sClass" : "middle jtox-multi", 
+        return { "sClass" : "center middle jtox-multi", 
                  "mData" : "effects", 
                  "sDefaultContent": "-", 
                  "mRender" : function(data, type, full) { return self.renderMulti(data, type, full, function(data, type) { return data.conditions[c]; } )} 
@@ -97,20 +120,27 @@ var jToxStudy = {
       
       // add also the "default" effects columns
       colDefs.push(
-        { "sClass": "middle jtox-multi", "mData": "effects", "mRender": function (data, type, full) { return self.renderMulti(data, type, full, "endpoint");  } },   // Effects columns
-        { "sClass": "middle jtox-multi", "mData" : "effects", "mRender": function (data, type, full) { return self.renderMulti(data, type, full, self.formatResult) } }
+        { "sClass": "center middle jtox-multi", "mData": "effects", "mRender": function (data, type, full) { return self.renderMulti(data, type, full, "endpoint");  } },   // Effects columns
+        { "sClass": "center middle jtox-multi", "mData" : "effects", "mRender": function (data, type, full) { return self.renderMulti(data, type, full, self.formatResult) } }
       );
       
       // finally put the protocol entries
       colDefs.push(
         { "sClass": "center", "mData": "protocol.guidance", "mRender" : "[,]", "sDefaultContent": "?"  },    // Protocol columns
-        { "sClass": "center", "mData": "uuid", "sDefaultContent": "?" }
+        { "sClass": "center", "mData": "uuid", "bSearchable": false, "sDefaultContent": "?" }
       );
       
       // READYY! Go and prepare THE table.
       $(theTable).dataTable( {
         "bPaginate": true,
         "aoColumns": colDefs,
+        "sDom" : "rt<Fip>",
+        "fnInfoCallback": function( oSettings, iStart, iEnd, iMax, iTotal, sPre ) {
+          var el = $('.jtox-study-title', $(this).parentsUntil('.jtox-study')[0].parentNode)[0];
+          var str = el.innerHTML;
+          el.innerHTML = str.replace(/(.+)\s\(([0-9]+)\)/, "$1 (" + iTotal + ")");
+          return sPre;
+        }
       });
     }
     else
@@ -123,27 +153,36 @@ var jToxStudy = {
     
   },
   
-  processStudies: function (tab, study) {
+  processStudies: function (tab, study, map) {
     var self = this;
     var cats = [];
     
     // first swipe to map them to different categories...
     for (var i = 0, slen = study.length; i < slen; ++i) {
       var ones = study[i];
-      if (cats[ones.protocol.category] === undefined) {
-        ones['index'] = 1;
-        cats[ones.protocol.category] = { "name": ones.protocol.endpoint, "array" : [ones]};
+      if (map) {
+        if (cats[ones.protocol.category] === undefined) {
+          ones['index'] = 1;
+          cats[ones.protocol.category] = { "name": ones.protocol.endpoint, "array" : [ones]};
+        }
+        else {
+          ones['index'] = cats[ones.protocol.category].array.length + 1;
+          cats[ones.protocol.category].array.push(ones);
+        }
       }
-      else {
-        ones['index'] = cats[ones.protocol.category].array.length + 1;
-        cats[ones.protocol.category].array.push(ones);
-      }
+      else  // i.e. - don't remap - just add the index
+        ones['index'] = i + 1;
     }
+
+    // add this one, if we're not remapping. map == false assumes that all passed studies will be from
+    // one category.    
+    if (!map)
+      cats[study[0].protocol.category] = { "name" : study[0].protocol.endpoint, "array" : study };
     
     // now iterate within
     for (var c in cats) {
       var onec = cats[c];
-      self.ensureCategory(tab, c, onec.name, onec.array.length);
+      self.ensureCategory(tab, c, onec.name);
       var theTable = self.ensureTable(tab, onec.array[0]);
       $(theTable).dataTable().fnAddData(onec.array);
     }
@@ -158,7 +197,8 @@ var jToxStudy = {
 
 $(document).ready(function(){
   $('#jtox-studies').tabs();
-  jToxStudy.processStudies(document.getElementById('jtox-pchem'), toxStudies.study);
+  
+  jToxStudy.processStudies(document.getElementById('jtox-pchem'), toxStudies.study, true);
 });
 
 
