bdcd63cac9de19b115dca7259623693ad248a305
Ivan Georgiev <ivan@jonan.info>
2014-04-07 13:34:38 +0300

    - [matrix] first sketch-up of the initial tab.

--- scripts/toxmatrix.js
+++ scripts/toxmatrix.js
@@ -5,14 +5,25 @@
 **/
 
 var jToxMatrix = {
+	createForm: null,
+	root: null,
+	settings: { // defaults settings go here
+		
+	},
+	
 	init: function (root, settings) {
+		var self = this;
+
+		self.root = root;
+    self.settings = $.extend(self.settings, jT.settings, settings);
+		
     var loadPanel = function(panel){
       if (panel){
       }
     };
     
     // initialize the tab structure for several versions of dataTables.
-    jT.$(root).tabs({
+    $(root).tabs({
     	"disabled": [1, 2, 3, 4, 5],
       "select" : function(event, ui) {
         loadPanel(ui.panel);
@@ -23,6 +34,20 @@ var jToxMatrix = {
       }
     });
     
-    jT.$('.jq-buttonset', root).buttonset();
+    $('.jq-buttonset', root).buttonset();
+    
+    self.createForm = $('.jtox-assessment form', root)[0];
+    self.createForm.assFinalize.style.display = 'none';
+    self.createForm.assUpdate.style.display = 'none';
+    self.createForm.assDuplicate.style.display = 'none';
+    
+    // finally, if provided - load the given assessmentUri
+    if (!ccLib.isNull(self.settings.assessmentUri)) {
+	    self.load(self.settings.assessmentUri);
+    }
+	},
+	
+	load: function(assessmentUri) {
+		
 	},
 };
