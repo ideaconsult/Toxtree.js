cd0c6093717fa31295c83c3cc1549efd50109f76
Ivan Georgiev <thejonan@gmail.com>
2014-12-23 10:39:01 +0200

    - [matrix] Issue #86

--- uses/config-dataset.js
+++ uses/config-dataset.js
@@ -1,5 +1,32 @@
 jTConfig.dataset = {
   "baseFeatures": {
+		"http://www.wikipathways.org/index.php/Pathway" :  {
+			"title": "Wiki Pathways",
+			"data" : "compound.wikipathway",
+			"accumulate": true,
+			"render" : function(data, type, full) {
+				return (type != "display") ? data : full.compound.wikipathway;
+			}
+		},
+		"http://www.opentox.org/echaEndpoints.owl#Carcinogenicity" : {
+      "title": "Carcinogenicity",
+      "data" : "compound.carcinogenicity",
+      "accumulate": true,
+      "render" : function(data, type, full) {
+        return (type != "display") ? data : (
+                (data=="active")?('<span style="color:red">' + data + '</span>'):data
+        );
+      }
+    },
+		"http://www.opentox.org/echaEndpoints.owl#Mutagenicity" : {
+			"title": "Mutagenicity",
+      "data" : "compound.mutagenicity",
+			"accumulate": true,
+			"render" : function(data, type, full) {
+        return (type != "display") ? data : (
+				  (data=="active")?('<span style="color:red">' + data + '</span>'):data);
+      }
+		},
 		"http://www.opentox.org/api/1.1#CompositionInfo" : {
 		  "visibility": "details",
 			"title": "Composition",
